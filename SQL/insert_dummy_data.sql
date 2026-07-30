333122f065d147d87dd19efb461932fdeab18d49
mubsharhusnain <mubsharhusnain550@gmail.com>
2026-07-30 18:15:21 +0500

    Update insert_dummy_data.sql

--- SQL/insert_dummy_data.sql
+++ SQL/insert_dummy_data.sql
@@ -1,1 +1,16 @@
+-- ==========================================
+-- AWS Project 3
+-- Insert Sample Records
+-- Author: Mubshar Husnain
+-- ==========================================
 
+USE InternshipDB;
+
+INSERT INTO Interns (InternID, FirstName, LastName, Email)
+VALUES
+    (1, 'John', 'Doe', 'jdoe@decodelabs.com'),
+    (2, 'Jane', 'Smith', 'jsmith@decodelabs.com'),
+    (3, 'Conan', 'O''Leary', 'coleary@decodelabs.com');
+
+-- Verify Inserted Records
+SELECT * FROM Interns;
