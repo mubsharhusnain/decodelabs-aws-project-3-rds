95eb5df32eeb5ba8a6c4bd7abb9aa2796e9c9a99
mubsharhusnain <mubsharhusnain550@gmail.com>
2026-07-30 18:15:05 +0500

    Update create_table.sql

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -1,1 +1,22 @@
+-- ==========================================
+-- AWS Project 3
+-- Amazon RDS MySQL Database
+-- Create Interns Table
+-- Author: Mubshar Husnain
+-- ==========================================
 
+CREATE DATABASE IF NOT EXISTS InternshipDB;
+
+USE InternshipDB;
+
+CREATE TABLE Interns (
+    InternID INT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Verify Table
+SHOW TABLES;
+
+DESCRIBE Interns;
